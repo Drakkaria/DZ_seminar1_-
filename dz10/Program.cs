708145d307da8cc9ce73a286d8831f7969455e47
akaya <darkness_ftv@mail.ru>
2022-12-17 03:02:40 +0800

    dz10

---
+++ dz10/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 10. Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому. Если второе число не кратно числу первому, 
+// то программа выводит остаток от деления.
+
+Console.Write("введите 1е число");
+Double FN = Double.Parse(Console.Readline()!);
+
+Console.WriteLine("введите 2е число");
+Double SN = Double.Parse(Console.Readline()!);
+
+if (FN % SN == 0)
+{
+    Console.Write("1е число кратно 2му");
+    }
+
+else
+{
+    Console.Write("вывод остатка");
+    Console.Write("FN/SN");
+}
+
