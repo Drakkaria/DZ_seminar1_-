f70d182487cc0c1349276a94dbe77e1a08fdfe76
akaya <darkness_ftv@mail.ru>
2022-12-16 23:11:55 +0800

    dz5

--- dz5/Program.cs
+++ dz5/Program.cs
@@ -1,1 +1,6 @@
 ﻿// 5. Показать последнюю цифру трёхзначного числа.
+
+Console.WriteLine("Введите трех значное число");
+int a = int.Parse(Console.ReadLine()!);
+int b = a % 10;
+Console.WriteLine(b);
