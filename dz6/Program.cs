bc3235e2b1ef40a5b611348a86cf2c3809121fab
akaya <darkness_ftv@mail.ru>
2022-12-16 23:20:32 +0800

    dz6

--- dz6/Program.cs
+++ dz6/Program.cs
@@ -1,1 +1,7 @@
 ﻿// Показать вторую цифру трёхзначного числа.
+
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a/10%10;
+Console.Write("второе число: ");
+Console.WriteLine(b);
