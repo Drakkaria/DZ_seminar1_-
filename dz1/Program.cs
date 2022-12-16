372caa608f1a7aad3ae71cade1296eebb76affce
akaya <darkness_ftv@mail.ru>
2022-12-16 18:12:45 +0800

    Решила 1ю задачу

---
+++ dz1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите первое число ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Проверим явлется ли первое число квадратом второго ");
+if (b*b==a){
+     Console.WriteLine("Первое число явлется квадратом второго ");
+}
+else{
+    Console.WriteLine("Первое число не явлется квадратом второго ");
+}
+
+
+
+ 
+
