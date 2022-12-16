edd067bd0aa6e0323cb3292b660384c9c90a7156
akaya <darkness_ftv@mail.ru>
2022-12-16 20:11:26 +0800

    Решила задачу2

---
+++ dz2/Program.cs
@@ -0,0 +1,42 @@
+﻿// 2. По заданному номеру дня недели вывести его название.
+
+Console.Write("Введите число номер дня недели ");
+int day = int.Parse(Console.ReadLine()!);
+
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+int f = 6;
+int g = 7;
+
+if(day == a)
+{
+    Console.WriteLine ("понедельник");
+}
+if(day == b)
+{
+    Console.WriteLine ("вторник");
+}
+if(day == c)
+{
+    Console.WriteLine ("среда");
+}
+if(day == d)
+{
+    Console.WriteLine ("четверг");
+}
+if(day == e)
+{
+    Console.WriteLine ("пятница");
+}
+if(day == f)
+{
+    Console.WriteLine ("суббота");
+}
+if(day == g)
+{
+    Console.WriteLine ("воскресение");
+}
+
