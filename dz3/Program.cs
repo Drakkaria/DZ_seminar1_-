eff72b8f36a24e5e40f776c9b9d623536c6b08a6
akaya <darkness_ftv@mail.ru>
2022-12-20 19:24:34 +0800

    dz3

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -1,14 +1,19 @@
-﻿// 3. Найти максимальное из трех чисел.
+﻿// Задача №3. 
+// Найти максимальное из трех чисел.
+
 Console.WriteLine("Введите первое число: ");
 int a = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите второе число: ");
 int b = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите третье число: ");
 int с = int.Parse(Console.ReadLine()!);
+
 int max = a;
+
 if (a > max) max = a;
 if (b > max) max = b;
 if (с > max) max = с;
-Console.WriteLine("max = ");
-Console.WriteLine(max);
+
+string str = $"Максимальное число = {max}." ;
+Console.WriteLine(str);
 
