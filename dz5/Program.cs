1ec2bfd08b350cb833c34b7885a862ac51655c22
akaya <darkness_ftv@mail.ru>
2022-12-21 23:36:16 +0800

    dz5

--- dz5/Program.cs
+++ dz5/Program.cs
@@ -1,6 +1,17 @@
-﻿// 5. Показать последнюю цифру трёхзначного числа.
+﻿// Задача №5. 
+// Показать последнюю цифру трёхзначного числа.
 
-Console.WriteLine("Введите трех значное число");
+
+Console.Write("Здрасти! Введите пожалуйста трёхзначное число: ");
 int a = int.Parse(Console.ReadLine()!);
 int b = a % 10;
-Console.WriteLine(b);
+
+if (a>99&&a<1000)
+{
+    // Console.Write("Последня цифра: ");
+    // Console.Write(b);
+    string str = $"Последняя цифра: {b}";
+    Console.Write(str);
+
+}
+else Console.Write("Это не трех значное число, алло!");
