2c3c90f88324fb6fc033137da69854d8cee0fc93
akaya <darkness_ftv@mail.ru>
2022-12-21 23:30:47 +0800

    dz4

--- dz4/Program.cs
+++ dz4/Program.cs
@@ -5,10 +5,7 @@ Console.Write("Введите число: ");
 int a = int.Parse(Console.ReadLine()!);
 
 if (a%2==0) // При делении на 2 остаток будет равено 0. 4/2=2 (остаок 0) 5/2=2.5 (остаток 5)
-{
-    Console.WriteLine("четное число ");
-}
+Console.WriteLine("четное число ");
+
 else
-{
-    Console.WriteLine("нечетное число ");
-}
+Console.WriteLine("нечетное число ");
