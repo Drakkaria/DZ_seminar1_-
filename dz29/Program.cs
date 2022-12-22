18c548f586f2b3482070657305a5650552b70982
akaya <darkness_ftv@mail.ru>
2022-12-23 00:33:49 +0800

    dz29

--- dz29/Program.cs
+++ dz29/Program.cs
@@ -3,3 +3,16 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+
+
+int[]  array = new int[8];
+Console.Write("[");
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+    string elements = Convert.ToString(array[i]);
+    int convert = Convert.ToInt32(elements);
+    Console.Write(convert.ToString() + ", ");
+}
+Console.Write("]");
