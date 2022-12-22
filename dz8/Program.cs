204e85b29d3c2f3f647ce996dfd9f85fb4606f1c
akaya <darkness_ftv@mail.ru>
2022-12-22 15:34:27 +0800

    dz8

--- dz8/Program.cs
+++ dz8/Program.cs
@@ -1,7 +1,17 @@
 ﻿// Удалить вторую цифру трёхзначного числа.
 
+//Console.WriteLine("Введите трехзначное число");
+//int a = int.Parse(Console.ReadLine()!);
+//int b = a / 100 * 10 + a % 10;  // 589/100=5, 589%10=9, 
+//Console.WriteLine(b);
+
 Console.WriteLine("Введите трехзначное число");
 int a = int.Parse(Console.ReadLine()!);
-int b = a / 100 * 10 + a % 10;
-Console.WriteLine(b);
-
+int b = a / 100;
+int c = a % 10;
+if (a > 99 && a < 1000) 
+{
+Console.Write(b);
+Console.Write(c);
+}
+else Console.Write("Алло, читай вниметельно да! Это не трехзначное число!");
