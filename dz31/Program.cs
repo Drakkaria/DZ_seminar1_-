b6d8ba531f78546f0ae369c5d70c95120b88563b
akaya <darkness_ftv@mail.ru>
2022-12-23 02:48:05 +0800

    dz31

--- dz31/Program.cs
+++ dz31/Program.cs
@@ -3,20 +3,22 @@
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
 
 int[] array = new int [10];
+
 Console.Write("[");
 for (int i = 0; i <array.Length; i++)
 
 {
-    int randomNumber = new Random().Next(-9, 9);
+    int randomNumber = new Random().Next(-9, 10);
     array [i] = randomNumber;
     Console.Write(array [i].ToString() + ", ");
     
 }
 Console.Write("] -> ");
+
 Console.Write("[");
-for (int i = 0; i <array.Length; i++)
+for (int i = 0; i < array.Length; i++)
 {
-    array [i] = (i * -1);
+    array [i] = (array [i] * -1);
     Console.Write(array [i].ToString() + ", ");
 }
 Console.Write("]");
