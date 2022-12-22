d7165400ed0d9b1cad9db0fb4aa90053267ea914
akaya <darkness_ftv@mail.ru>
2022-12-22 15:39:31 +0800

    dz8

--- dz8/Program.cs
+++ dz8/Program.cs
@@ -2,7 +2,7 @@
 
 //Console.WriteLine("Введите трехзначное число");
 //int a = int.Parse(Console.ReadLine()!);
-//int b = a / 100 * 10 + a % 10;  // 589/100=5, 589%10=9, 
+//int b = a / 100 * 10 + a % 10;  // 589/100=5, 5*10+50, + 589%10=9, 50+9=59
 //Console.WriteLine(b);
 
 Console.WriteLine("Введите трехзначное число");
